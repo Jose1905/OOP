d70c07f51e387d278aa0e1f63c6f264334052570
jose1905 <davarela231@gmail.com>
2023-06-15 21:58:34 -0600

    Prepare Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapeList = new List<Shape>();
+
+        Square newSquare = new Square("Red", 4);
+        shapeList.Add(newSquare);
+
+        Circle newCircle = new Circle("Blue", 4);
+        shapeList.Add(newCircle);
+
+        Rectangle newRectangle = new Rectangle("Green", 4, 6);
+        shapeList.Add(newRectangle);
+
+        for(int i = 0; i < shapeList.Count(); i ++){
+            Console.WriteLine(shapeList[i].GetColor());
+            Console.WriteLine(shapeList[i].GetArea());
+        }
     }
 }
